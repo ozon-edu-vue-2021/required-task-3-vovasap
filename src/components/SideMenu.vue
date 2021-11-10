94d6264fd3dd9013820e2db87363ac4125ddd6de
vovasap <rednotgreen2020@gmail.com>
2021-11-10 08:26:28 +0300

    feat: add diagram

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -32,7 +32,7 @@
           <span v-else class="legend--empty"> Список пуст </span>
         </div>
         <div class="legend__chart">
-          <!-- chart -->
+          <Pie ref="chart" />
         </div>
       </div>
       <div v-else class="profile">
@@ -49,6 +49,7 @@ import LegendItem from "./SideMenu/LegendItem.vue";
 import PersonCard from "./SideMenu/PersonCard.vue";
 import legend from "@/assets/data/legend.json";
 import Draggable from "vuedraggable";
+import { Pie } from "vue-chartjs";
 
 export default {
   props: {
@@ -65,6 +66,7 @@ export default {
     LegendItem,
     PersonCard,
     Draggable,
+    Pie,
   },
   data() {
     return {
@@ -74,12 +76,34 @@ export default {
   created() {
     this.loadLegend();
   },
+  mounted() {
+    this.makeChart();
+  },
   methods: {
     loadLegend() {
       this.legend = legend;
     },
     closeProfile() {
       this.$emit("update:isUserOpenned", false);
+    },
+    makeChart() {
+      const chartData = {
+        labels: this.legend.map((legendItem) => legendItem.text),
+        datasets: [
+          {
+            label: "Легенда",
+            backgroundColor: this.legend.map((legendItem) => legendItem.color),
+            data: this.legend.map((legendItem) => legendItem.counter),
+          },
+        ],
+      };
+      const options = {
+        legend: {
+          display: false,
+        },
+      };
+
+      this.$refs.chart.renderChart(chartData, options);
     },
   },
 };
